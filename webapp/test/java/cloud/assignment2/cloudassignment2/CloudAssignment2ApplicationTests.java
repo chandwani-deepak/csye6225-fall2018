42c61c5d76ab79551b8cf7250132f49e410c6b02
bhargavanaman <bhargava.n@northeastern.edu>
2018-09-27 13:32:29 -0400

    web app files

---
+++ webapp/test/java/cloud/assignment2/cloudassignment2/CloudAssignment2ApplicationTests.java
@@ -0,0 +1,16 @@
+package cloud.assignment2.cloudassignment2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudAssignment2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
