656df98630e43add3ae1d6dc43215bb0f97dbc9a
chandwani-deepak <chandwani.d@northeastern.edu>
2018-09-27 14:11:37 -0400

    typo corrected us-east-1 changed to us-east-1

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,4 +1,4 @@
-cho "Creating CloudFormation Stack"
+echo "Creating CloudFormation Stack"
 aws cloudformation create-stack --stack-name $1 --template-body file://csye-cf-networking.yaml --parameters file://startmyinstance-parameters.json
 
 aws cloudformation validate-template --template-body file://csye-cf-networking.yaml
