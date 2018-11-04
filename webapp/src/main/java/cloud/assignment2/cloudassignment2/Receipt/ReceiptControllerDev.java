fcbd8fdba9ebeb8e6931c8cbc4b41f976797fb59
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-04 01:45:44 -0400

    trying to run travis from dA78 change in file upload receipt2

--- webapp/src/main/java/cloud/assignment2/cloudassignment2/Receipt/ReceiptControllerDev.java
+++ webapp/src/main/java/cloud/assignment2/cloudassignment2/Receipt/ReceiptControllerDev.java
@@ -71,7 +71,7 @@ public class ReceiptControllerDev {
                                     .build();
 
                             //s3Client.putObject(new PutObjectRequest(bucketName, fileName, new File("/home/deepakchandwani/Downloads/"+file.getOriginalFilename())).withCannedAcl(CannedAccessControlList.PublicRead));
-				 s3Client.putObject(new PutObjectRequest(bucketName, fileName, file.getInputStream(),new ObjectMetadata())
+				 s3Client.putObject(new PutObjectRequest(bucketName, fileName, file)
 					.withCannedAcl(CannedAccessControlList.PublicRead));
 
                         }
