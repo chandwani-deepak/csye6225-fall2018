8e80b60329459f4359f91a2d30a1095d4d09652f
chandwani-deepak <chandwani.d@northeastern.edu>
2018-11-08 03:34:22 -0500

    49

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -44,7 +44,6 @@ echo "Fetching VPC details"
 VPC_ID=$(aws ec2 describe-vpcs --query Vpcs[0].VpcId --output text)
 
 echo "Fetching domain name from Route 53"
-echo "Fetching domain name from Route 53"
 DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
 DOMAIN_NAME="${DOMAIN_NAME%?}"
 echo "$DOMAIN_NAME"
@@ -85,15 +84,17 @@ STACKDETAILS=$(aws cloudformation describe-stacks --stack-name $1-application --
 echo "Application stack creation complete"
 echo "Application Stack id: $STACKDETAILS"
 
-aws cloudformation create-stack --stack-name $1-serverless --template-body file://./csye6225-cf-serverless.json
-aws cloudformation wait stack-create-complete --stack-name $1-serverless
+LAMBDABUCKET="lambda.$DOMAIN_NAME"
+echo "LAMBDA_BUCKET:- $LAMBDABUCKET"
+
 AccountId=$(aws iam get-user|python -c "import json as j,sys;o=j.load(sys.stdin);print o['User']['Arn'].split(':')[4]")
 echo "AccountId: $AccountId"
-aws lambda add-permission \
-        --function-name "csye6225-fall2018-lambda" \
-        --action 'lambda:InvokeFunction' \
-        --principal events.amazonaws.com \
-        --source-arn "arn:aws:sns:us-east-1:352915658406:SNSTopicResetPassword"
+
+SNSTOPIC_ARN="arn:aws:sns:us-east-1:$AccountId:SNSTopicResetPassword"
+echo "SNSTOPIC_ARN: $SNSTOPIC_ARN"
+
+aws cloudformation create-stack --stack-name $1-serverless --capabilities "CAPABILITY_NAMED_IAM" --template-body file://./csye6225-cf-serverless.json --parameters ParameterKey=LAMBDABUCKET,ParameterValue=$LAMBDABUCKET ParameterKey=SNSTOPICARN,ParameterValue=$SNSTOPIC_ARN
+aws cloudformation wait stack-create-complete --stack-name $1-serverless
 STACKDETAILS=$(aws cloudformation describe-stacks --stack-name $1-serverless --query Stacks[0].StackId --output text)
 
 exit 0
